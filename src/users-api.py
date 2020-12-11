06f5346b11cc463eaabbf1c73c51f51f10694835
klemenStanic <ks4211@student.uni-lj.si>
2020-12-11 14:23:31 +0100

    bf

--- src/users-api.py
+++ src/users-api.py
@@ -37,8 +37,8 @@ app.config['SECRET_KEY'] = 'TOP-SECRET_KEY'
 logger = logging.getLogger("logstash")
 logger.setLevel(logging.INFO)        
 
-log_endpoint_uri = environ["LOGS_URI"]
-log_endpoint_port = environ["LOGS_PORT"]
+log_endpoint_uri = str(environ["LOGS_URI"]).strip()
+log_endpoint_port = int(environ["LOGS_PORT"].strip())
 
 
 # Create the handler
