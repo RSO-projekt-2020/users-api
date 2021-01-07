887115d54e1fe8cd55eb469de142eba3726e196b
Luka Kavčič <luka.kavcic95@gmail.com>
2021-01-07 21:30:49 +0100

    typo

--- src/users-api.py
+++ src/users-api.py
@@ -313,7 +313,7 @@ def search():
     if query is None:
         return make_response({'msg': 'empty query', 'content': []})
     logger.info("[user-api] someone is querying for users like:" + str(query))
-    users = User.query.filter_by(email=User.email.like(query)).all()
+    users = User.query.filter_by(User.email.like(query)).all()
     return make_response({'msg': 'ok', 'content': [x.to_dict() for x in users]})
 
 
